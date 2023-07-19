5fe1561cac84f409dfbf4c2124f216cf029936b4
xgost <xgost@belikesoftware.com>
2023-07-19 19:46:44 +0200

    :art: Design system incorporated

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,14 @@
 import React from "react"
 import './infrastructure/i18n'
+import { ChakraProvider } from '@chakra-ui/react'
 import Header from "./components/Header"
 
 const App: React.FC = () => {
 
   return (
+    <ChakraProvider>
       <Header /> 
+    </ChakraProvider>
   )
 }
 export default App
--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import Settings from "../infrastructure/settings";
+import { Box, Heading, Text } from "@chakra-ui/react";
 
 const Header: React.FC = () => {
 
   const { t } = useTranslation();
   return (
-    <header>
-      <h1>{Settings.appName()}</h1>
-      <h2>{t('description')}</h2>
-    </header>
+    <Box as='header'>
+      <Heading as='h1'>
+      {Settings.appName()}
+      </Heading>
+      <Text>{t('description')}</Text>
+    </Box>
+    
   )
 }
 
